d1f676f6b8ece5e9ba144e28febc3fcfd05deb2e
MichaelGreason <mgreason18@gmail.com>
2023-03-07 15:29:22 -0500

    created views, urls, and templates for the homepage, and add books page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Resources
+from .forms import ResourcesForm
 
-# Create your views here.
+
+def list_books(request):
+    books = Resources.objects.all()
+    return render(request, 'books/index.html', {'books': books})
+
+
+def add_book(request):
+    if request.method == 'POST':
+        resources_form = ResourcesForm(request.POST)
+    if resources_form.is_valid():
+        resources_form.save()
+        return redirect('home')
+    form = ResourcesForm()
+    return render(request, 'books/add_book.html', {'form': form})
--- django_freeshelf/urls.py
+++ django_freeshelf/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from books import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.list_books, name='home'),
+    path("book/new", views.add_book, name="add_book"),
 ]
